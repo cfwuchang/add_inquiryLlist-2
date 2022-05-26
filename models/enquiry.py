5c73902f5a5805484832171deda662449c6436ac
cfwuchang <67139173+cfwuchang@users.noreply.github.com>
2022-05-26 14:35:50 +0800

    Update enquiry.py

--- models/enquiry.py
+++ models/enquiry.py
@@ -5,8 +5,8 @@ class Enquiry(models.Model):
 
     x_many2many = fields.Many2many('purchase.order.line',domain="[('partner_id', '=', '线下采购')]",string=u'多选')
 
-    @api.constrains('x_many2many')
-    def _get_order(self):
-        for i in self.x_many2many:
-            if i:
-                self.write({'order_line':self.x_many2many,'x_many2many':None})
+#     @api.constrains('x_many2many')
+#     def _get_order(self):
+#         for i in self.x_many2many:
+#             if i:
+#                 self.write({'order_line':self.x_many2many,'x_many2many':None})
